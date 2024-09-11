0310d82e0b0d2b4afcb56bd7d4ea0130fe86528f
Kgothalang Lekitlane <isaacspeedy101@gmail.com>
2024-09-11 18:47:51 +0200

    NEW COMMITS

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,21 @@
 
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
-    <section>
+    <section className="about">
+       <div className ="about-content">
       <h2>About Us</h2>
       <p>
         GreatAfrica Cosmetics is dedicated to providing high-quality, natural, and organic beauty products. 
         We believe in the power of nature to enhance your beauty and well-being. Our products are sourced 
         from the finest ingredients found in Africa, ensuring they are pure and effective.
       </p>
+    <p>
+      Founded By [owner] ,we strive to deliver exceptional quality and authenticity in every product.Join us on a journey to discover the beauty secrets of Africa
+    </p>
+    </div>
     </section>
   );
 };
